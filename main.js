8cc6211ec14604ad99edd600c10427b3ff4f4dfc
vaibhav7000 <142111005+vaibhav7000@users.noreply.github.com>
2023-12-25 19:24:49 +0530

    added index.html

---
+++ main.js
@@ -0,0 +1,30 @@
+const totaldownloads = document.getElementById('downloads');
+const totalClients = document.getElementById('clients');
+const totalThemes = document.getElementById('themes');
+const totalMembers = document.getElementById('total-members');
+
+let downloads = 200;
+let clients = 100;
+
+let themes = 400;
+let teamMembers = 100;
+
+// function counterIncrease(){
+//   for(let i=1;i<=90000;i++){
+//     totaldownloads.innerText=i;
+//   }
+
+//   for(let i=1;i<=clients;i++){
+//     totalClients.innerText=i;
+//   }
+  
+//   for(let i=1;i<=themes;i++){
+//     totalThemes.innerText=i;
+//   }
+  
+//   for(let i=1;i<=downloads;i++){
+//     totalMembers.teamMembers=i;
+//   }
+// }
+
+// counterIncrease();
